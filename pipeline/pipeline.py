af3a910ea2bd96ac15668499cbc83d0c31b08aa2
gshubham46 <gshubhamkumar46@gmail.com>
2026-05-04 07:10:15 -0400

    updated pipeline with correct settings

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,9 +1,9 @@
 import subprocess
 
 print("Step 1: Generating data...")
-subprocess.run(["python", "python/generate_data.py"])
+subprocess.run(["python", "python/data_generation.py"])
 
 print("Step 2: Loading data into PostgreSQL...")
-subprocess.run(["python", "python/load_to_db.py"])
+subprocess.run(["python", "python/load_data.py"])
 
 print("Pipeline completed successfully.")
