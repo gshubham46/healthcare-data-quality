31a56cc12ce55c9d1cf5a8ad4b63536ab65ebcf9
gshubham46 <gshubhamkumar46@gmail.com>
2026-05-03 20:32:09 -0400

    Scaled data to 50K from 2K

--- python/data_generation.py
+++ python/data_generation.py
@@ -6,7 +6,7 @@ import random
 fake = Faker()
 
 records = []
-for _ in range(2000):
+for _ in range(50000):
     records.append({
         "first_name": fake.first_name(),
         "last_name": fake.last_name(), 
